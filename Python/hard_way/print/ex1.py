a4a195fc3c984cc8c673afb9c1a098dd45254981
= <=>
2018-05-20 23:23:28 +0530

    started basic python print statement tutorials

---
+++ Python/hard_way/print/ex1.py
@@ -0,0 +1,7 @@
+#Just messing around for the first time here.....
+
+print("Hello Python!!")
+print("Howdy??")
+print("AHAHAHAHAH!!!")
+
+print("What is One plus Two ??? It's ", 1+2)
---
+++ Python/hard_way/print/ex4.py
@@ -0,0 +1,11 @@
+print("Mon\nTue\nWed\nThur\nFri\nSat\nSun")
+
+print("""
+Junaid Arham Zaki
+What are you upto exactly??
+Ohh I SEE.... 
+(0) | (0)
+This way is easier to print multiple lines 
+""")
+
+print "Heyyy"
